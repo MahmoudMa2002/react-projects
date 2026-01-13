a742f10faeeef44f1326f1f8b315f10dc2b8718b
Mahmoud <mazenmahmod201@gmail.com>
2026-01-13 13:48:44 +0300

    Update logo image source path in Header component

--- Travel-Journal/src/components/Header.jsx
+++ Travel-Journal/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header (){
     return (
         <header>
-            <img src="/public/logo.png" alt="Planet Logo" />
+            <img src="/logo.png" alt="Planet Logo" />
             <h1> my travel journal.</h1>
         </header>
     )
